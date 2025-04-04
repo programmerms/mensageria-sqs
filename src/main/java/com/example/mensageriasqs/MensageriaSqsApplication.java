65ead3eca2f2c0979b9fd6afdb15c1e9da438c60
molivio <programmerms@gmail.com>
2025-04-04 07:55:00 -0400

    Initial commit

---
+++ src/main/java/com/example/mensageriasqs/MensageriaSqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mensageriasqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaSqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MensageriaSqsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mensageriasqs/MensageriaSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mensageriasqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
